275a4353aa00c27b9bb06857e725251ab4004f3f
kjige <chrisleongsc2@gmail.com>
2017-06-21 17:27:27 -0500

    OK

--- controller/routes.js
+++ controller/routes.js
@@ -55,7 +55,7 @@ module.exports = function(app) {
     });
 
     app.get("/api/resume", function(req, res){
-        console.log(req);
+        res.send('OK');
     });
 
     app.get("*", function(req, res) {
