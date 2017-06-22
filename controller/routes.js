4c7fb8fe0e5d09a6e7ec5aa4d09aa1b6320b6614
kjige <chrisleongsc2@gmail.com>
2017-06-21 21:54:05 -0500

    answers

--- controller/routes.js
+++ controller/routes.js
@@ -57,8 +57,25 @@ module.exports = function(app) {
     app.get("/api/resume", function(req, res){
         if (req.query.q == 'Ping'){
             res.send('OK');
+        } else if (req.query.q == 'Name'){
+            res.send('Chris Leong');
+        } else if (req.query.q == 'Years'){
+            res.send('Less than one');
+        } else if (req.query.q == 'Degree'){
+            res.send('Northwestern University Coding Bootcamp')
+        } else if (req.query.q == 'Referrer'){
+            res.send('LinkedIn')
+        } else if (req.query.q == 'Position'){
+            res.send('Software Engineer')
+        } else if (req.query.q == 'Email Address'){
+            res.send('chrisleongsc2@gmail.com')
+        } else if (req.query.q == 'Phone'){
+            res.send('913-963-7598')
+        } else if (req.query.q == 'Source'){
+            res.send('http://github.com/kjige')
+        
         } else {
-            res.send('Yes');
+            res.send('');
         }
     });
 
